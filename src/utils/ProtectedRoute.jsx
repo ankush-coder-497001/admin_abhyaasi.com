8c592fab77d3405ab7ee2b1d24821d7f236b670d
Ankush Kumar Gupta <167736782+ankush-coder-497001@users.noreply.github.com>
2026-01-28 16:07:48 +0530

    fix: redirection issue while logging

--- src/utils/ProtectedRoute.jsx
+++ src/utils/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = () => {
-  const { isLoggedIn } = useSelector((state) => state.auth);
+  const isLoggedIn = localStorage.getItem("abhyaasi_admin_token") ? true : false;
   return isLoggedIn ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
